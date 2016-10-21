86226d0e8b0e41b0f9a4113fa4ba87b74eeaaf11
nehameena45 <neha.meena@valuelabs.com>
2016-10-21 18:02:10 +0530

    Admin Module Code

---
+++ NGOUserPage/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin_NGO.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult Active_Users()
+        {
+            return View();
+        }
+
+        public ActionResult Blocked_Users()
+        {
+            return View();
+        }
+
+        public ActionResult Requests()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
