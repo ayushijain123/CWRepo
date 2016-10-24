3425910dfb97235d1587c1e64bde8aa4b21132d5
Rishi-ValueLabs <rishiraj.malvi@valuelabs.com>
2016-10-24 15:59:04 +0530

    User Registration Changes

--- NGOUserPage/Controllers/userRegistrationController.cs
+++ NGOUserPage/Controllers/userRegistrationController.cs
@@ -20,22 +20,29 @@ namespace NGOUserPage.Controllers
         {
             if (ModelState.IsValid)
             {
-                CommonWealEntities1 context = new CommonWealEntities1();
-                UserLogin obj = new UserLogin();
-                obj.LoginPassword = ru.UserPassword;
-                obj.LoginEmailID = ru.UserEmail;
-                var roleobj = context.RoleTypes.Where(w => w.RoleName == "USER").FirstOrDefault();
-                obj.LoginUserType = roleobj.RoleID;
-                obj.IsActive = true;
-                obj.IsBlock = false;
-                obj.ModifiedOn = DateTime.Now;
-                obj.CreatedOn = DateTime.Now;
-                context.UserLogins.Add(obj);
-                context.SaveChanges();
-                ru.LoginID = obj.LoginID;
-                context.RegisteredUsers.Add(ru);
-                context.SaveChanges();
-               
+                try
+                {
+                    CommonWealEntities1 context = new CommonWealEntities1();
+                    UserLogin obj = new UserLogin();
+                    obj.LoginPassword = ru.UserPassword;
+                    obj.LoginEmailID = ru.UserEmail;
+                    var roleobj = context.RoleTypes.Where(w => w.RoleName == "USER").FirstOrDefault();
+                    obj.LoginUserType = roleobj.RoleID;
+                    obj.IsActive = true;
+                    obj.IsBlock = false;
+                    obj.ModifiedOn = DateTime.Now;
+                    obj.CreatedOn = DateTime.Now;
+                    context.UserLogins.Add(obj);
+                    context.SaveChanges();
+                    ru.LoginID = obj.LoginID;
+                    ru.LoginUserType = 3; // Added by Rishiraj  on 24/10/2016
+                    context.RegisteredUsers.Add(ru);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
             }
            
             return RedirectToAction("CreateUserPost","userRegistration");
