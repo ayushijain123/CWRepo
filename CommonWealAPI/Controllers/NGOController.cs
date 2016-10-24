81445578908a0eb2f4ea1c09b29553a2824f2a1c
Rishi-ValueLabs <rishiraj.malvi@valuelabs.com>
2016-10-24 18:33:05 +0530

    Web API Code

---
+++ CommonWealAPI/Controllers/NGOController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using NGOUserPage;
+
+namespace CommonWealAPI.Controllers
+{
+    public class NGOController : ApiController
+    {
+
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            CommonWealEntities1 db = new CommonWealEntities1();
+            var objTest = db.Tests.Where(x => x.ID == 1).FirstOrDefault();
+            var response = Request.CreateResponse(HttpStatusCode.OK, objTest);
+            return response;
+        }
+
+        [HttpPost]
+        public bool PostTest(Test objTest)
+        {
+            CommonWealEntities1 db = new CommonWealEntities1();
+            db.Tests.Add(objTest);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
