ba1886c056a753009d591066841fe597e29b58c6
hemant-patidar <hemant.patidar@valuelabs.com>
2016-10-26 21:21:27 +0530

    Implemented Base controller and Authorization

--- CommonWeal.NGOWeb/Global.asax.cs
+++ CommonWeal.NGOWeb/Global.asax.cs
@@ -30,19 +30,8 @@ namespace CommonWeal.NGOWeb
 
         protected void Application_AuthenticateRequest(Object sender, EventArgs e)
         {
-            HttpCookie authCookie =
-                          Context.Request.Cookies[FormsAuthentication.FormsCookieName];
-            if (authCookie != null)
-            {
-                FormsAuthenticationTicket authTicket =
-                                            FormsAuthentication.Decrypt(authCookie.Value);
-                string[] roles = authTicket.UserData.Split(new Char[] { ',' });
-                GenericPrincipal userPrincipal =
-                                 new GenericPrincipal(new GenericIdentity(authTicket.Name), roles);
-                Context.User = userPrincipal;
-            }
+
         }
-
-
     }
+
 }
