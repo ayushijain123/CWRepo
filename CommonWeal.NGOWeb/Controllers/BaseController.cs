01008434b9ae99ea8a7b85f0fe08bb61cf765cc5
hemant-patidar <hemant.patidar@valuelabs.com>
2016-11-03 12:28:06 +0530

    Added UserName

--- CommonWeal.NGOWeb/Controllers/BaseController.cs
+++ CommonWeal.NGOWeb/Controllers/BaseController.cs
@@ -7,13 +7,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using CommonWeal.NGOWeb;
+using CommonWeal.NGOWeb.ViewModel;
+using CommonWeal.NGOWeb.Utility;
 
 namespace CommonWeal.NGOWeb.Controllers
 {
     [Authorize]
     public class BaseController : Controller
     {
-        public User LoggedInUser { get; set; }
+        public LoggedInUser LoginUser { get; set; }
         /// <summary>
         /// Fetch logged in user detail
         /// </summary>
@@ -25,8 +27,37 @@ namespace CommonWeal.NGOWeb.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 CommonWealEntities1 CWContext = new CommonWealEntities1();
-                this.LoggedInUser = CWContext.Users.Where(user => user.LoginEmailID.ToLower() == HttpContext.User.Identity.Name.ToLower() && user.IsActive && !user.IsBlock).FirstOrDefault();
-                ViewBag.LoggedInUser = LoggedInUser;
+
+                var usr = CWContext.Users.Where(user => user.LoginEmailID.ToLower() == HttpContext.User.Identity.Name.ToLower() && user.IsActive && !user.IsBlock).FirstOrDefault();
+
+
+                this.LoginUser = new ViewModel.LoggedInUser()
+                {
+                    LoginID = usr.LoginID,
+                    LoginEmailID = usr.LoginEmailID,
+                    LoginUserType = usr.LoginUserType,
+
+
+                };
+
+
+                switch ((TypeHelper.UserType)usr.LoginUserType)
+                {
+                    case TypeHelper.UserType.Admin:
+                        this.LoginUser.UserName = "Admin";
+                        break;
+                    case TypeHelper.UserType.NGOAdmin:
+
+                        this.LoginUser.UserName = CWContext.NGOUsers.Where(user => user.LoginID == this.LoginUser.LoginID).FirstOrDefault().NGOName; ;
+                        break;
+                    case TypeHelper.UserType.User:
+                        var reguser = CWContext.RegisteredUsers.Where(user => user.LoginID == this.LoginUser.LoginID).FirstOrDefault(); ;
+                        this.LoginUser.UserName = reguser.LastName + " " + reguser.FirstName;
+                        break;
+                }
+
+
+                ViewBag.LoginUser = LoginUser;
             }
 
         }
