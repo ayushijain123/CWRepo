4619988a8237d63274001f4d0290fa1f404fcc70
ayushijain123 <ayushi.jain@valuelabs.com>
2016-11-04 15:05:55 +0530

    postlikemodel update

---
+++ CommonWeal.NGOWeb/ViewModel/PostLikeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommonWeal.NGOWeb.ViewModel
+{
+    public class PostLikeModel
+    {
+        public string userName { get; set; }
+        public string userImageUrl { get; set; }
+        public int  UserID { get; set; }
+    }
+}
