4b335e11173db73d0314b0c4bb48483125a6cfd8
pandeypooja <pooja.pandey@valuelabs.com>
2016-11-08 18:53:50 +0530

    prevented null entry of otp

--- CommonWeal.NGOWeb/Models/Password.cs
+++ CommonWeal.NGOWeb/Models/Password.cs
@@ -6,7 +6,8 @@ namespace CommonWeal.NGOWeb.Models
     public class Password
     {
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail")]
-        [Required(ErrorMessage = "This field is required")]
+       // [Required(ErrorMessage = "This field is required")]
+        [Required(ErrorMessageResourceType = typeof(ModelMessages), ErrorMessageResourceName = "Generic_Required")]
         public string UserEmail { get; set; }
 
         [RegularExpression(@"^.*(?=.{8,12})(?=.*\d)(?=.*[a-zA-Z]).*$", ErrorMessageResourceType = typeof(ModelMessages), ErrorMessageResourceName = "Generic_passwordStrength")]
@@ -16,5 +17,8 @@ namespace CommonWeal.NGOWeb.Models
         [Compare("NewPassword")]
         [Required(ErrorMessageResourceType = typeof(ModelMessages), ErrorMessageResourceName = "Generic_Required")]
         public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ModelMessages), ErrorMessageResourceName = "Generic_Required")]
+        public string FinalOTP { get; set; }
     }
 }
